fe12c8b42e7aca23596192ee762cf07c475d3639
Craig <foutscw@gmail.com>
2026-03-31 09:59:29 +0200

    Minor changes

--- C/src/main.c
+++ C/src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    printf("Hello, World!");
+void test(int a[][4], int len) {
+    for (int i = 0; i < len; ++i)
+        for (int j = 0; j < 4; ++j)
+            printf("%d\n", a[i][j]);
+}
+
+int main(void) {
+    int f[][4] = {
+        {1, 4, 3, 2},
+        {4, 6, 1, 6}
+    };
+
+    test(f, 2);
 
     return 0;
 }
